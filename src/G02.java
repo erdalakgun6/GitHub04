b03ed68f2097fd18ec4cdee374ff266515089e0e
erdalakgun6 <erdalakgun6@gmail.com>
2023-03-29 17:23:08 +0300

    4.gün

---
+++ src/G02.java
@@ -0,0 +1,11 @@
+public class G02 {
+    /*
+    Git-GitHub'da projelerimizin üzerinde calisirken temelde 3 adimda islem yapiyoruz
+        1- projelerimizi/Dosyalrimizi git ile iliskilendiriyoruz
+                git-init
+        2- projemizde/dosyamizda bulunanlari version olusturuyorduk
+
+                git add .
+                git commit -m"versionadi"
+     */
+}
